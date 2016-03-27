76d9b09d1a6fa2a21d5b7478e6eb5e5217eed88c
jhad7010 <jhad7010@gmail.com>
2016-03-27 09:52:13 -0600

    Initial Commit

---
+++ PruebaGit3Tests/PruebaGit3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PruebaGit3Tests.swift
+//  PruebaGit3Tests
+//
+//  Created by Jaime Haddad on 3/27/16.
+//  Copyright © 2016 Jaime Haddad. All rights reserved.
+//
+
+import XCTest
+@testable import PruebaGit3
+
+class PruebaGit3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
